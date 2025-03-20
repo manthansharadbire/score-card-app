38dda5f2d6129c352134669d0c271c1b4cae9c00
Manthan Bire <manthansharadbire@gmail.com>
2025-03-20 17:05:18 +0530

    basic design for the scorecard has been accomplished

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,26 @@ import React from 'react';
 
 function App() {
   return (<>
-    <div>
-    <h1 className='text-center text-2xl m-3'>La-Liga Scorecard</h1>
+    <div className='border border-gray-700 m-4 shadow-lg rounded-xl bg-slate-700 text-white '>
+    <h1 className='text-center text-2xl m-3 font-sans'>Football Scorecard</h1>
     </div>
 
-    <div className='flex justify-around'>
-    <div className='w-1/3 bg-blue-500'>
-    <h1>Barcelona</h1>
+    <div className='flex justify-evenly'>
+    <div className='w-1/3 bg-slate-700 text-center shadow-lg rounded-xl text-white'>
+    <h1 className='text-center text-2xl m-2 border-b pb-1'>Barcelona</h1>
     <span className='text-9xl'>0</span>
-    <div>
-  <span className='text-7xl'>-</span>
-  <span className='text-7xl'>+</span>
+    <div className='text-center'>
+  <span className='text-7xl mx-7 text-red-600 '>-</span>
+  <span className='text-7xl mx-7 text-green-500'>+</span>
     </div>
     </div>
     
-    <div className='w-1/3 bg-blue-500'>
-    <h1>Barcelona</h1>
-    <span className='text-9xl'>0</span>
-    <div className='justify-around'>
-  <span className='text-7xl'>-</span>
-  <span className='text-7xl'>+</span>
+    <div className='w-1/3 bg-slate-700 text-center shadow-lg rounded-xl text-white'>
+    <h1 className='text-center text-2xl m-2 border-b pb-1'>Arsenal</h1>
+    <span className='text-9xl text-center'>0</span>
+    <div className='m-3 text-center'>
+  <span className='text-7xl mx-7 text-red-600'>-</span>
+  <span className='text-7xl mx-7 text-green-500'>+</span>
     </div>
 
     </div>
